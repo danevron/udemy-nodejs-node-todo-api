8947ff0ae1b528b3e0196751be4c12e19954f064
Dan Evron <danevron@gmail.com>
2018-05-06 13:55:05 +0300

    Adds test for index route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,11 +4,19 @@ const request = require('supertest');
 const app = require('./../server');
 const { Todo } = require('./../models/todo');
 
-describe('POST /todos', () => {
+const testTodos = [{
+  text: 'first test todo'
+}, {
+  text: 'second test todo'
+}];
 
-  beforeEach((done) => {
-    Todo.remove({}).then(() => done());
-  });
+beforeEach((done) => {
+  Todo.remove({}).then(() => {
+    return Todo.insertMany(testTodos);
+  }).then(() => done());
+});
+
+describe('POST /todos', () => {
 
   it('creates and save a todo', (done) => {
     const text = 'test text';
@@ -24,7 +32,7 @@ describe('POST /todos', () => {
           return done(err);
         }
 
-        Todo.find().then((todos) => {
+        Todo.find({text}).then((todos) => {
           expect(todos.length).toBe(1);
           expect(todos[0].text).toBe(text);
           done();
@@ -43,9 +51,21 @@ describe('POST /todos', () => {
         }
 
         Todo.find().then((todos) => {
-          expect(todos.length).toBe(0);
+          expect(todos.length).toBe(2);
           done();
         }).catch(e => done(e));
       });
+  });
+});
+
+describe('GET /todos', () => {
+  it('returns all todos', (done) => {
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todos.length).toBe(2);
+      })
+      .end(done);
   });
 });
