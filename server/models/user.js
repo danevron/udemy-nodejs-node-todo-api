359df313264aa5f1fb6820e6b7ea658f70c03132
Dan Evron <danevron@gmail.com>
2018-05-08 14:03:01 +0300

    Adds and generate tokens on user creation

--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,52 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
+const _ = require('lodash');
+const jwt = require('jsonwebtoken');
 
-const User = mongoose.model('User', {
+let userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
-    minlength: 1,
-    trim: true
-  }
+    trim: true,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: '{VALUE} is not a valid email'
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+  tokens: [{
+    access: {
+      type: String,
+      required: true
+    },
+    token: {
+      type: String,
+      required: true
+    }
+  }]
 });
+
+
+userSchema.methods.toJSON = function () {
+  return _.pick(this.toObject(), ['_id', 'email']);
+};
+
+userSchema.methods.generateAuthToken = function () {
+  const access = 'auth';
+  var token = jwt.sign({access, _id: this._id.toHexString()}, 'abc123').toString();
+
+  this.tokens.push({access, token});
+
+  return this.save().then(() => {
+    return token;
+  });
+};
+
+const User = mongoose.model('User', userSchema);
 
 module.exports = { User };
