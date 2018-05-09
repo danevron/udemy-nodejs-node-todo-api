fb5a657c78558608814e8ed9125be04a5a5c5ee3
Dan Evron <danevron@gmail.com>
2018-05-09 23:54:11 +0300

    Adds tests to login route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,6 +4,7 @@ const { ObjectID } = require('mongodb');
 
 const app = require('./../server');
 const {Todo} = require('./../models/todo');
+const {User} = require('./../models/user');
 const {todos, populateTodos, users, populateUsers} = require('./seed/seed');
 
 beforeEach(populateTodos);
@@ -233,4 +234,46 @@ describe('POST /users', () => {
       .expect(400)
       .end(done);
   })
+});
+
+describe('POST /users/login', () => {
+  it('returns auth token on valid credentials', (done) => {
+    request(app)
+      .post('/users/login')
+      .send({
+        email: users[1].email,
+        password: users[1].password
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.headers['x-auth']).toExist();
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        };
+
+        User.findById(users[1]._id).then((user) => {
+          expect(user.tokens[0]).toInclude({
+            access: 'auth',
+            token: res.headers['x-auth']
+          });
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+
+  it('returns 400 on invalid credentials', (done) => {
+    request(app)
+      .post('/users/login')
+      .send({
+        email: 'one@example.com',
+        password: 'wrong!!'
+      })
+      .expect(400)
+      .expect((res) => {
+        expect(res.headers['x-auth']).toNotExist();
+      })
+      .end(done);
+  });
 });
